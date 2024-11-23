b207121c15005cff9d0f90a38ba7c17a0f053d1f
Mahesh Bhandari <n31mahesh@gmail.com>
2024-11-23 22:12:18 +0545

    update functionality remain

--- reduxToolkitTodo/src/Components/Todos.jsx
+++ reduxToolkitTodo/src/Components/Todos.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {deleteTodo, setInput, updateTodo} from '../Features/Todo/todoSlice'
+import {deleteTodo, setInput} from '../Features/Todo/todoSlice'
 
 function Todos() {
     const todos = useSelector(state => state.todos)
