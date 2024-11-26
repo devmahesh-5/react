3dc8a6a645bd8850bcbdb86ddbb41dad91437593
Mahesh Bhandari <n31mahesh@gmail.com>
2024-11-26 20:58:18 +0545

    authentication completed

---
+++ MegaBlog/src/Appwrite/auth.js
@@ -0,0 +1,44 @@
+import conf from "..conf/conf"
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;//as a method account cant be defined before client method setendpoint and setproject are defined
+    constructor(){
+        this.client.setEndpoint(conf.AppWriteEndpoint)
+       .setProject(conf.AppWriteProjectId);
+        this.account=new Account(this.client);
+    }
+   async createUser({email,password,name}){
+        try {
+            const user=await this.account.create(ID.unique(),email,password,name);
+            user?this.login({email,password}):user;
+        } catch (error) {
+            throw error;
+        }
+   }
+   async login({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password);
+        } catch (error) {
+            throw error;
+        }
+   }
+   async getCurrentUser(){
+    try{
+        return await this.account.get();
+    }catch(error){
+        throw error;
+    }
+    return null;
+}
+    async logout(){
+        try {
+            await account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite::logout error",error);
+        }
+    }
+    }
+ const authService=new AuthService();
+ export default authService;
--- MegaBlog/src/conf/conf.js
+++ MegaBlog/src/conf/conf.js
@@ -1,7 +1,7 @@
 import App from "../App"
 
 const conf={
-    AppWriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    AppWriteEndpoint:String(import.meta.env.VITE_APPWRITE_URL),
     AppWriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     AppWriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     AppWriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
