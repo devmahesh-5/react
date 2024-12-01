6a221da893123f57a72658fb3ad6142ae453cbd4
Mahesh Bhandari <n31mahesh@gmail.com>
2024-12-01 12:46:42 +0545

    Post form using rte with controller to get control of rte in postcard

--- MegaBlog/src/Components/post-form/Postform.jsx
+++ MegaBlog/src/Components/post-form/Postform.jsx
@@ -1,13 +1,116 @@
-import React,{useCallback} from 'react'
+import React, { useCallback } from 'react'
 import services from '../../Appwrite/config';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import {useSelector} from 'react-redux';
-import {Input,Button,Select} from '..';
-function Postform({post}) {
-    const {register,handleSubmit,}=useForm();
+import { useSelector } from 'react-redux';
+import { Input, Button, Select } from '..';
+function Postform({ post }) {
+    const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
+        defaultValues: {
+            title: post?.title || '',
+            content: post?.content || '',
+            status: post?.status || 'active',
+        }
+    });
+    const navigate = useNavigate();
+    const userData = useSelector((state) => state.auth.userData);
+    const submit = async (data) => {
+        if (post) {
+            const file = data.image[0] ? await services.uploadFile(data.image[0]) : null;
+            if (file) {
+                await services.deleteFile(post.featuredimage);
+            }
+            const updatedPost = await services.updatePost(post.$id, {
+                ...data,
+                featuredimage: file ? file.$id : undefined
+            });
+            if (updatedPost) {
+                navigate(`/post/${post.$id}`);
+            }
+        } else {
+            file = await services.uploadFile(data.image[0]);
+            if (file) {
+                data.featuredimage = file.$id;
+                const newPost = await services.createPost({
+                    ...data,
+                    userid: userData.$id//userData is the all data of user who is logged in which is accesed by getCurrentUser function and stored in authslice from login component
+                });
+                if (newPost) {
+                    navigate(`/post/${newPost.$id}`);
+                }
+            }
+        }
+    }
+
+    const slugTransform = useCallback((value) => {
+        if (value && typeof value === 'string')
+            return value
+                .trim()
+                .toLowerCase()
+                .replace(/[^a-zA-z\d ]+/g, '')
+        return '';
+    }, [])
+    React.useEffect(() => {
+        const subscription = watch((value,{name}) => {//for optimization we use the variable and later unsubscribe it 
+            //here value is value of input field and name is name of input field
+                if(name==='title'){
+                    setValue('slug',slugTransform(value.title,
+                        {shouldValidate: true}
+                    ));//set value of slug to slugTransform function and value of title as argument
+                }
+        });
+        return ()=>{
+            subscription.unsubscribe()
+        };
+    },[watch,setValue,slugTransform]);
     return (
-        <div>test</div>
+        <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+        <div className="w-2/3 px-2">
+            <Input
+                label="Title :"
+                placeholder="Title"
+                className="mb-4"
+                {...register("title", { required: true })}
+            />
+            <Input
+                label="Slug :"
+                placeholder="Slug"
+                className="mb-4"
+                {...register("slug", { required: true })}
+                onInput={(e) => {
+                    setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                }}//now whenever we enter space in slug input field it changes to - under the hood and changes state
+            />
+            <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
+        </div>
+        <div className="w-1/3 px-2">
+            <Input
+                label="Featured Image :"
+                type="file"
+                className="mb-4"
+                accept="image/png, image/jpg, image/jpeg, image/gif"
+                {...register("image", { required: !post })}
+            />
+            {post && (
+                <div className="w-full mb-4">
+                    <img
+                        src={appwriteService.getFilePreview(post.featuredImage)}
+                        alt={post.title}
+                        className="rounded-lg"
+                    />
+                </div>
+            )}
+            <Select
+                options={["active", "inactive"]}
+                label="Status"
+                className="mb-4"
+                {...register("status", { required: true })}
+            />
+            <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                {post ? "Update" : "Submit"}
+            </Button>
+        </div>
+    </form>
     )
 }
 
